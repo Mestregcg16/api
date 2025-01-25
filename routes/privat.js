ea651970d0b504db2f1602ba7d7ffea8b38d207e
Lucas Matheus <86500439+Mestregcg16@users.noreply.github.com>
2025-01-24 22:24:39 -0300

    Início do server

---
+++ routes/privat.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const router = express.Router()
+const prisma = new PrismaClient()
+
+router.get('/listar-usuarios', async (req, res) => {
+    try {
+        const user = await prisma.user.findMany({ omit: { password: true}})
+       
+        res.status(200).json({ message: 'USUARIOS LISTADOS', user})
+    } catch (error) {
+        res.status(500).json({ message:'FALHA NO SERVIDOR'})
+    }
+})
+//CATALOGO
+router.get('/catalogo', async (req, res) => {
+    try {
+        const jogos = await prisma.user.findMany()
+    } catch (error) {
+        res.status(500).json({message: 'não foi possiel listar os jogos'})
+    }
+})
+router.get('/JogoDaVelha', async (req, res) => {
+    
+})
+
+export default router
---
+++ routes/public.js
@@ -0,0 +1,88 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const prisma = new PrismaClient()
+const router = express.Router()
+const jwt_secret = process.env.JWT_SECRET
+const app = express()
+
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Configuração do __dirname para módulos ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Rota para enviar o arquivo HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html')); // Substitua 'index.html' pelo nome do seu arquivo
+});
+
+
+//      CAMINHO DOS ARQUIVOS
+app.use(express.static('../src'))
+
+//ROTA INICIAL
+router.get('/', async (req, res) =>{
+    
+})
+
+//CADASTRO
+router.post('/cadastro', async (req, res) => {
+    try{
+        const user = req.body
+    
+        const salt = await bcrypt.genSalt(10)//NIVEL DE DIICULDADE
+        const hashPassword = await bcrypt.hash(user.password, salt)//CRYPTOGRAFANDO COM A DIFICULDADE 10
+    
+        const ud = await prisma.user.create({
+            data: {
+                email:  user.email,
+                name:   user.name,
+                password:  hashPassword,
+                idade: user.idade,
+                cidade: user.cidade
+            }
+        })
+        res.status(201).json(ud)
+    }catch{
+        res.status(500).json( {menssage: "erro no servirdor"})
+    }
+
+})
+
+//ROTA DE LOGIN
+router.post('/login', async (req, res) => {
+    try {
+        const userInfo = req.body
+        //BUSCAR O USUARIO NO DB
+        const user = await prisma.user.findUnique({
+            where: {name: userInfo.name},
+        })
+
+        //VERIFICA SE EXISTE NO DB
+        if(!user) {
+            return res.status(404).json({ message: 'Usuario não encontrado'})
+        }
+
+        //COMPARA AS SENHAS DO DB COM A DO USUARIO
+        const ismash = await bcrypt.compare(userInfo.password, user.password)
+        if(!ismash){
+            res.status(400).json({message: 'senha invalida'})
+        }
+
+        //GERRAR O TOKEN JWT
+        const token = jwt.sign({id: user.id}, jwt_secret, {expiresIn: '1d'})
+
+        res.status(200).json(token)
+    } catch{
+        res.status(500)
+    }
+})
+
+
+
+export default router
